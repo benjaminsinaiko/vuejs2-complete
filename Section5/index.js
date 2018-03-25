d53e6edc49cb7515ba6bfa5375364653796ebff4
benjaminsinaiko <ben.jamin@outlook.com>
2018-03-25 15:07:44 -0500

    Look at , ,

--- Section5/index.js
+++ Section5/index.js
@@ -1,14 +1,17 @@
 /* global Vue */
+var data = {
+  title: 'The VueJS Instance',
+  showParagraph: false
+};
+
 var vm1 = new Vue({
   el: '#app1',
-  data: {
-    title: 'The VueJS Instance',
-    showParagraph: false
-  },
+  data: data,
   methods: {
     show: function() {
       this.showParagraph = true;
       this.updateTitle('The VueJS Instance (Updated)');
+      console.log((this.$refs.myButton = 'Test'));
     },
     updateTitle: function(title) {
       this.title = title;
@@ -25,6 +28,9 @@ var vm1 = new Vue({
     }
   }
 });
+
+console.log(vm1.$data === data);
+vm1.$refs.heading.innerText = 'Something else';
 
 setTimeout(function() {
   vm1.title = 'Changed by Timer';
