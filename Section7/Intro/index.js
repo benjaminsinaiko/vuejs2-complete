22ffd1e80528d8d28988fac59882309ab3b4917a
benjaminsinaiko <ben.jamin@outlook.com>
2018-03-29 12:10:16 -0500

    Story data in components, sharing same data object

--- Section7/Intro/index.js
+++ Section7/Intro/index.js
@@ -1,8 +1,18 @@
 /* global Vue */
-new Vue({
-  el: '#app',
-  data: {
-    status: 'Critical'
+var data = { status: 'Critical' };
+
+Vue.component('my-cmp', {
+  data: function() {
+    return data;
   },
-  template: '<p>Server Status: {{ status }}'
+  template:
+    '<p>Server Status: {{ status }} (<button @click="changeStatus">Change</button>)</p>',
+  methods: {
+    changeStatus: function() {
+      this.status = 'Normal';
+    }
+  }
+});
+new Vue({
+  el: '#app'
 });
