aaaa26d5b7ad94347c76d28173be30ce72e3fabc
benjaminsinaiko <ben.jamin@outlook.com>
2018-04-02 16:30:40 -0500

    Add components-assignments folder

--- Section7/components/src/App.vue
+++ Section7/components/src/App.vue
@@ -1,17 +1,10 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Components</h1>
-
-            </div>
-        </div>
+    <div>
+      <app-server-status></app-server-status>
     </div>
 </template>
 
 <script>
-    export default {
-    }
 </script>
 
 <style>
