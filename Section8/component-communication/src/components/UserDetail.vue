0437d5375de8bc9078ae2d17dcc91e78fb60487d
benjaminsinaiko <ben.jamin@outlook.com>
2018-04-03 12:12:16 -0500

    Add name props to send data from parent to child

--- Section8/component-communication/src/components/UserDetail.vue
+++ Section8/component-communication/src/components/UserDetail.vue
@@ -2,14 +2,18 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>User Name: {{ name }}</p>
     </div>
 </template>
 
 <script>
+export default {
+  props: ['name']
+};
 </script>
 
 <style scoped>
-    div {
-        background-color: lightcoral;
-    }
+div {
+  background-color: lightcoral;
+}
 </style>
