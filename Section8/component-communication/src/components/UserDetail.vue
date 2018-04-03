57f166ea8774c94760b66da85b2d611bbaa6d993
benjaminsinaiko <ben.jamin@outlook.com>
2018-04-03 12:24:40 -0500

    Validating props

--- Section8/component-communication/src/components/UserDetail.vue
+++ Section8/component-communication/src/components/UserDetail.vue
@@ -8,7 +8,13 @@
 
 <script>
 export default {
-  props: ['myName'],
+  props: {
+    myName: {
+      type: String,
+      // required: true
+      default: 'Benjamin'
+    }
+  },
   methods: {
     switchName() {
       return this.myName
