be6dd40187cb50fce5579190a26f8ecaa75a9d9c
benjaminsinaiko <ben.jamin@outlook.com>
2018-04-03 23:25:29 -0500

    Communicate between sibling components, edit age example

--- Section8/component-communication/src/components/UserDetail.vue
+++ Section8/component-communication/src/components/UserDetail.vue
@@ -3,6 +3,7 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User Name: {{ switchName() }}</p>
+        <p>User Age: {{ userAge }}</p>
         <button @click="resetName">Reset Name</button>
         <button @click="resetFn()">Reset Name</button>
     </div>
@@ -16,7 +17,8 @@ export default {
       // required: true
       default: 'Benjamin'
     },
-    resetFn: Function
+    resetFn: Function,
+    userAge: Number
   },
   methods: {
     switchName() {
