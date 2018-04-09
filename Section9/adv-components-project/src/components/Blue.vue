3ed9509e4ef5c62b3ceb9ebf7aee36b2377f0148
benjaminsinaiko <ben.jamin@outlook.com>
2018-04-09 16:19:38 -0500

    My solution

--- Section9/adv-components-project/src/components/Blue.vue
+++ Section9/adv-components-project/src/components/Blue.vue
@@ -1,17 +1,16 @@
 <template>
-    <div></div>
+    <div>I'm the Blue Component.</div>
 </template>
 
 <script>
-
 </script>
 
 <style scoped>
-    div {
-        border: 1px solid blue;
-        background-color: lightblue;
-        padding: 30px;
-        margin: 20px auto;
-        text-align: center
-    }
+div {
+  border: 1px solid blue;
+  background-color: lightblue;
+  padding: 30px;
+  margin: 20px auto;
+  text-align: center;
+}
 </style>
--- Section9/adv-components-project/src/components/Red.vue
+++ Section9/adv-components-project/src/components/Red.vue
@@ -1,17 +1,16 @@
 <template>
-    <div></div>
+    <div>Don't forget me!  I'm Red.</div>
 </template>
 
 <script>
-
 </script>
 
 <style scoped>
-    div {
-        border: 1px solid red;
-        background-color: lightcoral;
-        padding: 30px;
-        margin: 20px auto;
-        text-align: center
-    }
+div {
+  border: 1px solid red;
+  background-color: lightcoral;
+  padding: 30px;
+  margin: 20px auto;
+  text-align: center;
+}
 </style>
